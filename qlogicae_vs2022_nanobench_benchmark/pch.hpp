dee459e1cb600723384c4cc12596d7c2352e02b5
Vaughn Cedric L. Araneta <vaughncedriclipardoaraneta@gmail.com>
2025-11-02 03:03:17 +0800

    continued pch.hpp
    - implemented more built-in include headers

--- qlogicae_vs2022_nanobench_benchmark/pch.hpp
+++ qlogicae_vs2022_nanobench_benchmark/pch.hpp
@@ -2,8 +2,103 @@
 
 #include <nanobench.h>
 
+#include <any>
+#include <variant>
+#include <optional>
+#include <typeinfo>
+#include <typeindex>
+#include <type_traits>
+#include <compare>
+#include <source_location>
+
+#include <cstddef>
+#include <cstdint>
+#include <climits>
+#include <limits>
+#include <bit>
+#include <bitset>
+
+#include <string>
+#include <string_view>
+#include <charconv>
+#include <format>
+#include <regex>
+#include <cctype>
+#include <cwctype>
+
+#include <array>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <span>
+#include <tuple>
+#include <stack>
+#include <queue>
+#include <iterator>
+#include <ranges>
+
+#include <memory>
+#include <memory_resource>
+#include <new>
+
+#include <thread>
+#include <future>
+#include <mutex>
+#include <shared_mutex>
+#include <condition_variable>
+#include <atomic>
+#include <stop_token>
+#include <semaphore>
+#include <latch>
+#include <barrier>
+
+#include <chrono>
+#include <ratio>
 #include <random>
+#include <numbers>
+
+#include <fstream>
+#include <filesystem>
+
 #include <iostream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <iomanip>
+#include <ios>
+#include <cstdio>
+#include <cstdlib>
+
+#include <stdexcept>
+#include <cassert>
+#include <cerrno>
+#include <csignal>
+#include <cmath>
+#include <complex>
+#include <valarray>
+
+#include <algorithm>
+#include <functional>
+#include <execution>
+#include <locale>
+
+#include <version>
+
+#include <concepts>
+#include <coroutine>
+
+#include <cfenv>
+#include <cfloat>
+
+#include <cstdarg>
+#include <csetjmp>
+#include <cstring>
+#include <ctime>
+
 #include <windows.h>
 
 namespace QLogicaeVS2022NanobenchBenchmark
